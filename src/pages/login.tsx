623fd112f67edca661d35f1f1e99acad7850342e
Ujwal-Gaonkar <ujwalgaonkar07@gmail.com>
2024-09-21 19:36:04 +0530

    fixed dashbaord

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -48,7 +48,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:8000/api/auth/login', {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/login` , {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -40,7 +40,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:8000/api/users/register', {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/register`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
